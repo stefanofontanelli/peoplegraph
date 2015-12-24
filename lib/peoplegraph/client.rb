405804f5cbc7ce813a90008036dcfaabc5f05505
Edoardo Rossi <edd.rossi@gmail.com>
2015-12-24 11:06:10 +0100

    Add doc/comments

--- lib/peoplegraph/client.rb
+++ lib/peoplegraph/client.rb
@@ -6,6 +6,7 @@ require 'ostruct'
 require 'peoplegraph'
 
 module PeopleGraph
+  # rubocop:disable Metrics/LineLength
   # Client wraps a call to the `/lookup` endpoint.
   # Supported params:
   # - email
@@ -14,6 +15,76 @@ module PeopleGraph
   # - company
   # - webhook_url
   # - webhook_id
+  #
+  # The Client must be initialized with an API key that can be passed as
+  # parameter or taken from the ENV variable 'PEOPLEGRAPH_API_KEY'.
+  # There is also a reasonable default for the endpoint URL
+  # (https://api.peoplegraph.io) but you can provide a custom URL using
+  # the ENV variable 'PEOPLEGRAPH_API_URL'.
+  #
+  # An optional parameter for the client could be used (as an hash) to
+  # pass, some configurations to the Faraday connection
+  # used as HTTP client.
+  # For instance:
+  #
+  #      faraday_request_options = {
+  #        request: {
+  #          timeout: 20,
+  #          open_timeout: 20 } }
+  #
+  # The Client wraps only the /lookup endpoint and you can use it with
+  # the following helpers:
+  #
+  # - search_by_email
+  # - search_by_url
+  # - search_by_name
+  # - search_by_company
+  #
+  # Besides the obvious 'value' parameter, in the optional second
+  # parameter you can pass an hash with the instruction for the webhook.
+  # For instance:
+  #
+  #     options = {
+  #       webhook_id: 'foo',
+  #       webhook_url: 'http://posthere.foo.org' }
+  #
+  # This makes a huge difference in the kind of response you get from
+  # PeopleGraph and this from this client.
+  #
+  # Using a direct request you got this response with code 200 OK:
+  #
+  #     { "status"=>200,
+  #       "result"=>
+  #       { "name"=>"Edoardo Rossi",
+  #         "email"=>"edd.rossi@gmail.com",
+  #         "bio"=>
+  #           "Code monkey, GNU enthusiast, Avid reader, just a Geek, deep
+  #           Minimalist. #java #scala #ruby #js #python #geek #linux
+  #           #opensource",
+  #         "profiles"=>[
+  #           {"type"=>"gravatar", "url"=>"https://gravatar.com/zeroedd"},
+  #           {"type"=>"twitter", "url"=>"http://twitter.com/nulledd"},
+  #           {"type"=>"github", "url"=>"http://github.com/zeroed"}],
+  #         "avatars"=>[
+  #           "https://pbs.twimg.com/profile_images/577124934385692672/oIHT4sVk_400x400.jpeg",
+  #           "http://1.gravatar.com/avatar/2151b67a581590c280289086c3434d88?size=200"],
+  #         "websites"=>["http://zeroed.github.io"],
+  #         "locations"=>["Milan, Italy"]}}
+  #
+  # In this case, Client#search returns the "result" value into an easy
+  # OpenStruct (a "profile").
+  #
+  # Instead, using a webhook will gives you back this response with
+  # code 202 ACCEPTED:
+  #
+  #     { "status"=>202,
+  #       "message"=>
+  #         "We're checking this live to make sure you get the latest
+  #         information. Results will be posted to the provided webhook
+  #         URL in about 4 minutes. "}
+  #
+  # In this case, Client#search returns the whole hash.
+  # rubocop:enable Metrics/LineLength
   class Client
     attr_reader :api_key, :connection
 
